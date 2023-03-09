d98ffadf6eefffbea9759b93450a0b11b4a81b96
tomoya-y <yamanokuchi.tomoya.ys9@is.naist.jp>
2023-03-09 17:28:41 +0900

    追加

---
+++ run_multiple_sprite_ruch_model_naive.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+for i in `seq 5`
+do
+    docker run --rm --gpus all --privileged --net=host --ipc=host \
+    -e LOCAL_UID=$(id -u $USER) \
+    -e LOCAL_GID=$(id -g $USER) \
+    -v $HOME/.Xauthority:/home/$(id -un)/.Xauthority -e XAUTHORITY=/home/$(id -un)/.Xauthority \
+    -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY \
+    -v /dev/snd:/dev/snd -e AUDIODEV="hw:Device, 0" \
+    -v /home/$USER/workspace/C-DSVAE:/home/$USER/workspace/C-DSVAE \
+    -v /home/$USER/workspace/Controllable-C-DSVAE:/home/$USER/workspace/Controllable-C-DSVAE \
+    -v /home/$USER/workspace/Contrastively-Disentangled-Sequential-Variational-Audoencoder:/home/$USER/workspace/Contrastively-Disentangled-Sequential-Variational-Audoencoder \
+    -v /home/$USER/workspace/dataset:/home/$USER/workspace/dataset \
+    -v /hdd_mount/logs_cdsvae:/hdd_mount/logs_cdsvae \
+    docker_cdsvae python3.8 usecase/train/train.py \
+    --config-name=config_cdsvae_sprite_rich_model \
+    loss.weight.kld_context=1 \
+    loss.weight.kld_dynamics=1 \
+    loss.weight.contrastive_mi_fx=0.0 \
+    loss.weight.contrastive_mi_zx=0.0 \
+    loss.weight.mutual_information_fz=0.0 \
+    memo="naive"
+done
---
+++ run_multiple_stochastic_multiple_positive_cdsvae.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+
+for i in `seq 4`
+do
+    docker run --rm --gpus all --privileged --net=host --ipc=host \
+    -e LOCAL_UID=$(id -u $USER) \
+    -e LOCAL_GID=$(id -g $USER) \
+    -v $HOME/.Xauthority:/home/$(id -un)/.Xauthority -e XAUTHORITY=/home/$(id -un)/.Xauthority \
+    -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY \
+    -v /dev/snd:/dev/snd -e AUDIODEV="hw:Device, 0" \
+    -v /home/$USER/workspace/C-DSVAE:/home/$USER/workspace/C-DSVAE \
+    -v /home/$USER/workspace/Controllable-C-DSVAE:/home/$USER/workspace/Controllable-C-DSVAE \
+    -v /home/$USER/workspace/Contrastively-Disentangled-Sequential-Variational-Audoencoder:/home/$USER/workspace/Contrastively-Disentangled-Sequential-Variational-Audoencoder \
+    -v /home/$USER/workspace/dataset:/home/$USER/workspace/dataset \
+    -v /hdd_mount/logs_cdsvae:/hdd_mount/logs_cdsvae \
+    docker_cdsvae python3.8 usecase/train/train.py \
+    --config-name=config_cdsvae_dclaw_stochastic_multiple_positive
+done
